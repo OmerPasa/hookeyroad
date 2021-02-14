245e88931177c5adf216a95b9fb8e4442faf5373
OmerPasa3328 <omerfa33@yaani.com>
2021-02-15 00:13:12 +0300

    version changed

---
+++ Assets/instantiate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiate : MonoBehaviour
+{
+    public Transform tileObj;
+    private Vector3 nextBaseSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator spawnBase()
+    {
+        yield return new WaitForSeconds(1);
+        instantiate(tileObj, nextBaseSpawn, tileObj.rotation);
+    }
+}
