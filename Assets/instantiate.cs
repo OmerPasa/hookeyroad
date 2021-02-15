21de8e66d29c0fc4a14a937240b119440a71bd39
OmerPasa3328 <omerfa33@yaani.com>
2021-02-15 21:14:11 +0300

    just some tryes to run obstacles

--- Assets/instantiate.cs
+++ Assets/instantiate.cs
@@ -8,18 +8,29 @@ public class instantiate : MonoBehaviour
     private Vector3 nextBarsSpawn;
     public Transform barrier;
     private Vector3 nextbarrierSpawn;
-
+    private int randomZ;
     void Start()
     {
         nextBarsSpawn.x=24;
         nextBarsSpawn.y=4;
         StartCoroutine(spawnBars());
+        StartCoroutine(spawnbarrier());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+     IEnumerator spawnbarrier()
+    {
+        // left barrier
+        yield return new WaitForSeconds(1);
+        randomZ=Random.Range(-1,2);
+        nextbarrierSpawn.z=randomZ;
+        Instantiate(barrier, nextbarrierSpawn, barrier.rotation);
+        nextbarrierSpawn.x += 8;
+        StartCoroutine(spawnbarrier());
     }
     IEnumerator spawnBars()
     {
