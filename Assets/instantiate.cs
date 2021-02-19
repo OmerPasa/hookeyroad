e26a90d30d62bcf0a948a6d0b7f783432c997c97
OmerPasa3328 <omerfa33@yaani.com>
2021-02-19 21:10:25 +0300

    new changes in random like system random is added

--- Assets/instantiate.cs
+++ Assets/instantiate.cs
@@ -11,6 +11,7 @@ public class instantiate : MonoBehaviour
     private Vector3 nextbarrierleftSpawn;
     private Vector3 nextbarrierrightSpawn;
     private double randomZ;
+    public static System.Random ran = new System.Random();
     double[] barrierleftList = {1.23, 4.56, 7.89};
     double[] barrierrightList = {1.23, 4.56, 7.89};
     void Start()
@@ -32,9 +33,11 @@ public class instantiate : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         //randomZ=Random.Range(-1,2);
-        randomZ = Convert (barrierleftList[Random.Range(0, barrierleftList.Length)]);
-        //randomZ = barrierleftList[Random.Next(barrierleftList)];
-        randomZ= (double)nextbarrierleftSpawn.z;
+        //randomZ += 1.56; 
+        //randomZ = barrierleftList[Random.Next(barrierleftList.Length)];
+        randomZ = ran.Next(barrierleftList.Length);
+        //Randomizer.Randomize(barrierleftList);
+        nextbarrierleftSpawn.z = (float)randomZ;
         Instantiate(barrierleft, nextbarrierleftSpawn, barrierleft.rotation);
         nextbarrierleftSpawn.x += 8;
         StartCoroutine(spawnbarrierleft());
