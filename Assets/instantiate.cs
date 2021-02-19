d25f0c55c964f4326c4aea57fb461a7afb377491
OmerPasa3328 <omerfa33@yaani.com>
2021-02-19 17:44:07 +0300

    still working on

--- Assets/instantiate.cs
+++ Assets/instantiate.cs
@@ -32,8 +32,8 @@ public class instantiate : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         //randomZ=Random.Range(-1,2);
-        randomZ = barrierleftList[Random.Range(0, barrierleftList.Length)];
-        //randomZ = barrierleftList[Random.Next(barrierleftList)
+        randomZ = Convert (barrierleftList[Random.Range(0, barrierleftList.Length)]);
+        //randomZ = barrierleftList[Random.Next(barrierleftList)];
         randomZ= (double)nextbarrierleftSpawn.z;
         Instantiate(barrierleft, nextbarrierleftSpawn, barrierleft.rotation);
         nextbarrierleftSpawn.x += 8;
