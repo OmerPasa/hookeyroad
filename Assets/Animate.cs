9f635a433da21c466b1e9ee331cc6836c7709407
OmerPasa3328 <omerfa33@gmail.com>
2021-02-24 21:51:13 +0300

    Revert "commit"
    This reverts commit 99b58baccc97dd4f8e6a5b9fceb5525b648f0871.

--- Assets/Animate.cs
+++ Assets/Animate.cs
@@ -5,94 +5,15 @@ using UnityEngine;
 public class Animate : MonoBehaviour
 {
     Animator anim;
-    
-    bool rightslideUp;
-    const string PLAYER_IDLE = "Player_idle";
-    const string PLAYER_RUN = "Player_run";
-    const string PLAYER_JUMP = "Player_jump";
-    const string PLAYER_ATTACK = "Player_attack";
-    const string PLAYER_AIR_ATTACK = "Player_air_attack";
-
-
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void FixedUpdate()
-    {
-        //check if player is on the ground
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundMask);
-
-        if (hit.collider != null)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
-        //------------------------------------------
-
-        //Check update movement based on input
-        Vector2 vel = new Vector2(0, rb2d.velocity.y);
-
-        if (xAxis < 0)
-        {
-            vel.x = -walkSpeed;
-            transform.localScale = new Vector2(-1, 1);
-        }
-        else if (xAxis > 0)
-        {
-            vel.x = walkSpeed;
-            transform.localScale = new Vector2(1, 1);
-            
-        }
-        else
-        {
-            vel.x = 0;
-            
-        }
-
-        if (isGrounded && !isAttacking)
-        {
-            if (xAxis != 0)
-            {
-                ChangeAnimationState(PLAYER_RUN);
-            }
-            else
-            {
-                ChangeAnimationState(PLAYER_IDLE);
-            }
-        }
-
-        //------------------------------------------
-
-        //Check if trying to jump 
-        if (isJumpPressed && isGrounded)
-        {
-            rb2d.AddForce(new Vector2(0, jumpForce));
-            isJumpPressed = false;
-            ChangeAnimationState(PLAYER_JUMP);
-        }
-
-        //assign the new velocity to the rigidbody
-        rb2d.velocity = vel;
-    }
-void ChangeAnimationState(string newAnimation)
-    {
-        if (currentAnimaton == newAnimation) return;
-
-        animator.Play(newAnimation);
-        currentAnimaton = newAnimation;
     }
 }
