135efd53675bd415f1b599ff1d26b42a014677e1
OmerPasa3328 <omerfa33@gmail.com>
2021-05-06 18:56:01 +0300

    I hope we can find a solution to collusion

---
+++ Assets/Scripts/CrowCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowCollision : MonoBehaviour
+{
+    public crowbarmove crow;
+   void OnCollisinonEnter (Collision collisionInfo)
+   {
+       if (collisionInfo.collider.tag == "barrier")
+       {
+           Debug.Log("GAMEOVER");
+           GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+       }
+   }
+}
