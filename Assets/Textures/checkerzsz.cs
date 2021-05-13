40170fb3cc12abbe3bc648edcd6eb0fb7eb8b158
OmerPasa3328 <omerfa33@gmail.com>
2021-05-13 12:31:53 +0300

    trying to make it work

---
+++ Assets/Textures/checkerzsz.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkerzsz : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("anything", barrierleftV);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
